d9928249cd9e62358c5a97321c245aa9b0e68d9f
naderslim <naderslim@gmail.com>
2017-11-04 09:02:05 -0400

    updated variables

--- vagrant-setup-script/vagrant_vhosts.sh
+++ vagrant-setup-script/vagrant_vhosts.sh
@@ -1,7 +1,7 @@
 cp -R /var/www/vagrant-sites-config/* /etc/apache2/sites-available
 cd /etc/apache2/sites-available
-a2ensite local.cp1alt.com.conf
-a2ensite phpmyadmin.cp1alt.com.conf
+a2ensite local.PROJECT_NAME.com.conf
+a2ensite phpmyadmin.PROJECT_NAME.com.conf
 service apache2 reload
 service apache2 restart
 
